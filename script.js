2471f4d9a815db97e9c694a3ec2f5faa107c9d39
AKASH PANDEY <86301676+akash-2410@users.noreply.github.com>
2023-04-15 23:08:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let navbar=document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick =() =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+}
+let searchform=document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick =() =>{
+    searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+   
+}
+window.onscroll =() =>{
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+}
